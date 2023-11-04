feaada475ece1bc6b791bc1518740f68fd8f0aea
fernandopdc30 <91210708+fernandopdc30@users.noreply.github.com>
2023-11-03 23:39:04 -0500

    Update student file.h

--- student file.h
+++ student file.h
@@ -1,52 +1,72 @@
+#include <iostream>
+#include <cassert>
 #include <vector>
 #include <queue>
 
 using namespace std;
 
-class Tournament {
-public:
-    static vector<int> Torneo(vector<int> habilidades, int N, int K) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        int perdidas = 0;
-        int perdido;
-        int ganador;
+// Método: Torneo
+vector<int> Torneo(vector<int> habilidades, int N, int K) {
+    queue<int> cola;
+    vector<int> resultado(2);
 
-        for (int habilidad : habilidades) {
-            pq.push(habilidad);
-        }
-
-        while (true) {
-            int game = 0;
-            ganador = pq.top();
-            pq.pop();
-
-            while (!pq.empty()) {
-                perdido = pq.top();
-                pq.pop();
-
-                if (ganador == perdido) {
-                    continue;
-                }
-
-                game++;
-                if (ganador > perdido) {
-                    ganador += 2;
-                }
-
-                if (perdido == ganador - 2) {
-                    perdidas++;
-                }
-
-                if (game == K) {
-                    return {perdido, ganador};
-                }
-
-                break;
-            }
-
-            if (ganador > 0) {
-                pq.push(ganador - 2);
-            }
-        }
+    // Construir la cola de prioridad
+    priority_queue<int, vector<int>, greater<int>> colaPrioridad;
+    for (int habilidad : habilidades) {
+        colaPrioridad.push(habilidad);
     }
-};
+
+    int juegosGanados = 0;
+    int habilidadGanadora;
+
+    // Jugar juegos hasta llegar al K
+    while (juegosGanados < K) {
+        int habilidadPerdedora = colaPrioridad.top();
+        colaPrioridad.pop();
+
+        if (!colaPrioridad.empty()) {
+            habilidadGanadora = colaPrioridad.top();
+            colaPrioridad.pop();
+            juegosGanados++;
+
+            if (juegosGanados >= N) {
+                cola.push(habilidadGanadora);
+                juegosGanados = 0;
+            } else {
+                colaPrioridad.push(habilidadGanadora);
+            }
+        }
+
+        cola.push(habilidadPerdedora);
+    }
+
+    // Asignar habilidades del perdedor y ganador del juego K
+    resultado[0] = cola.front();
+    cola.pop();
+    resultado[1] = cola.front();
+
+    return resultado;
+}
+
+int main() {
+    // Prueba 1: Caso de prueba de ejemplo
+    vector<int> habilidades1 = {1, 2, 3};
+    int N1 = 2;
+    int K1 = 2;
+    vector<int> resultado1 = Torneo(habilidades1, N1, K1);
+    assert(resultado1[0] == 2);
+    assert(resultado1[1] == 3);
+
+    // Prueba 2: Caso de prueba con habilidades ordenadas en orden inverso
+    vector<int> habilidades2 = {3, 2, 1};
+    int N2 = 2;
+    int K2 = 2;
+    vector<int> resultado2 = Torneo(habilidades2, N2, K2);
+    assert(resultado2[0] == 2);
+    assert(resultado2[1] == 1);
+
+    // Agrega más casos de prueba aquí...
+
+    cout << "Todas las pruebas pasaron" << endl;
+    return 0;
+}
